33ea44c7eb87238dcf0b0d64ae510b9a5c2c85cb
John Michael <jmichaelten@gmail.com>
2020-01-30 11:59:27 -0500

    Interfaz de ruido total en enlace de subida y bajada terminado

--- ruido_antena.m
+++ ruido_antena.m
@@ -1,14 +1,11 @@
 
 
-function Ta = ruido_antena(tipo, Tg,gls,Ts,Tat,ag,al,Tw,w)
-    if tipo=="Con Lluvia"
+function Ta = ruido_antena(tipo,Tg,gls,Ts,Tat,ag,al,Tw,w)
+    if string(tipo)=="Con Lluvia"
     Ta = ((Tg*gls))/w + (Ts + Tat*(ag*al-1))*((1-gls)/(ag*al*w)) + (Tw*(w-1))/w;
-    elseif tipo=="Cielo Despejado"
+    elseif string(tipo)=="Cielo Despejado"
     Ta = ((Tg*gls))/w + (Ts + Tat*(ag-1))*((1-gls)/(ag*w)) + (Tw*(w-1))/w;
     else
-        continue
-    end
-    
-    
-   Ta=0    
+        xxx=0
+    end   
 end
--- ruido_total.m
+++ ruido_total.m
@@ -1,26 +1,17 @@
 
 
-function ruido = ruido_total(ruido_antena, ruido_recepcion,  tb)
-    to=290;
-    tb
-    if ruido_antena==0
+function [ruido_up ruido_down] = ruido_total(ruido_antena, ruido_recepcion,  temperatura_brillo)
+
         %el calculo es para up
 
-        %t=tb+t_recepcion
-    else
+        ruido_up =temperatura_brillo+ruido_recepcion;
+
         %el calculo es para downlink
-    %t=t_antena+t_recepcion
-    end
+        ruido_down=ruido_antena+ruido_recepcion;
+    
         disp('---------------------------------')
-        ruido=0
-    end
+
+ 
 
 end
 
-%vector de valores_g de g o l, pero en g.
-%en .super: organizar los valores g, si es l entonces valor[i]=1/l, sino valor[i]=g
-
-
-
-%si hay 4 elementos como se muestra en la figura:
-%to(l1-1)/1 + to(l2-1)/g1*1 + tamp3/g2*g1*1+ tamp4/g3*g2*g1*1
